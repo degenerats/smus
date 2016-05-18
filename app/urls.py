dbc9e951006c33d711dfac08f4ef972f0c3c608c
ravbetsky <ravbetsky@gmail.com>
2016-05-19 00:33:17 +0300

    auth and attendance

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^auth/', include('account.urls', namespace='account')),
+    url(r'^group/', include('core.urls', namespace='group')),
     url(r'^admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'page/base.html')
-
-def group(request):
-    return render(request, 'group/view.html')
